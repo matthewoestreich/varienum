a682329e5226e72e7b462ee3cba554f8559e31e5
Matt Oestreich <matthewpoestreich@gmail.com>
2026-04-19 19:59:04 -0500

    bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,7 @@
-extern crate proc_macro;
+pub use varienum_macro::VariantsVec;
 
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{DeriveInput, Meta, parse_macro_input};
-
-#[proc_macro_derive(VariantsVec, attributes(description))]
-pub fn variants_vec_derive(item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as DeriveInput);
-    let enum_name = input.ident;
-
-    let data = match input.data {
-        syn::Data::Enum(e) => e,
-        _ => panic!("VariantsVec can only be used on enums"),
-    };
-
-    let mut variants = Vec::new();
-    let mut variants_desc = Vec::new();
-
-    for variant in &data.variants {
-        let ident = &variant.ident;
-
-        // default empty description
-        let mut desc = String::new();
-
-        for attr in &variant.attrs {
-            if attr.path().is_ident("description")
-                && let Meta::NameValue(nv) = &attr.meta
-                && let syn::Expr::Lit(expr_lit) = &nv.value
-                && let syn::Lit::Str(lit_str) = &expr_lit.lit
-            {
-                desc = lit_str.value();
-            }
-        }
-
-        variants.push(ident.to_string());
-        variants_desc.push(quote! {
-            (stringify!(#ident), #desc)
-        });
-    }
-
-    let expanded = quote! {
-        impl varienum::VariantsVec for #enum_name {
-            fn variants() -> &'static [&'static str] {
-                &[#(#variants),*]
-            }
-
-            fn variants_desc() -> &'static [(&'static str, &'static str)] {
-                &[#(#variants_desc),*]
-            }
-        }
-    };
-
-    TokenStream::from(expanded)
+pub trait VariantsVec {
+    fn variants() -> &'static [&'static str];
+    fn variants_desc() -> &'static [(&'static str, &'static str)];
 }
+
